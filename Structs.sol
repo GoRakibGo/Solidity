52192898887c7ced0274919bd2edd5b7f9c1fa1b
Rakibul Islam <34100415+GoRakibGo@users.noreply.github.com>
2022-03-23 20:01:49 +0600

    Create Structs.sol

---
+++ Structs.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract Structs {
+    struct Car {
+        string model;
+        uint year;
+        address owner;
+    }
+    Car public car;
+    Car [] public cars;
+    mapping(address => Car[]) public carsByOwner;
+
+    function examples() external {
+        Car memory toyota = Car("Toyota", 1990, msg.sender);
+        Car memory lambo = Car({year: 1980, model: "Lamborghini", owner: msg.sender});
+        Car memory tesla;
+        tesla.model = "Tesla";
+        tesla.year = 2010;
+        tesla.owner = msg.sender;
+
+        cars.push(toyota);
+        cars.push(lambo);
+        cars.push(tesla);
+
+        cars.push(Car("Ferrari", 2020, msg.sender));
+
+        Car storage _car = cars[0];
+        _car.year = 1999;
+        delete _car.owner;
+
+        delete cars[1];
+    }
+}
